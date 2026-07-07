a0700dc355641a4b8ac16a91df0853c3931d33c2
andrade011 <santsandrade0@gmail.com>
2026-07-07 17:23:32 -0300

    upp

--- pensamento_projeto_larissa.py
+++ pensamento_projeto_larissa.py
@@ -8,7 +8,28 @@
 # print('Olá, Mundo!')
 
 
+# Inicializando as variáveis para o produto 1 (vazio)
+p1_nome = 'Açaí refresco'
+p1_estoque = 100
+p1_preco = 15.00
+p1_validade = 25/10/2026
+p1_descrição = "acaí comum, delicie-se com o rerefresco do nosso açaí"
 
+# Iniciando as variáveis para  produto 2 (vazio)
+p2_nome = 'Açaí premium'
+p2_estoque = 70
+p2_preco = 20.00
+p2_validade = 25/11/2026
+p2_descrição = "acaí premium, perfeito para quem gosta do melhor" 
+
+#iniciando as variáveis para  produto 3 (vazio)
+p3_nome = 'Açaí experience' 
+p3_estoque = 50
+p3_preco =30.00
+p3_validade = 25/10/2026
+p3_descrição = "acaí experience, ideial para matar aquela vontade de algo especial" 
+
+# Isso é um comentário de linha única
 
 print('_'* 48 + '\n')
 print('Bem vindo ao sistema de vendas - Açaiteria!\n')
@@ -25,14 +46,17 @@ print('0 - Sair do sistema')
 print('\n--------------------------------------\n')
 
 opcao__definida = int(input('Digite a opcao deseja: '))
+
 if opcao__definida == 1:
     print('Cadatrando produtos...')
-
+#faça a lógica para cadastrar o produto aqui,
+## e somente a inserção dos dados usando input e os tipos de dados.
+if p1_Açaí 
 elif opcao__definida == 2:
     print('Listando produtos...')
 
 elif opcao__definida == 3:
-    print('Realizando Vendas...')
+    print('Realizando Vendas...') 
 
 elif opcao__definida == 4:
     print('Preparando Pedido...') 
@@ -50,8 +74,10 @@ elif opcao__definida == 8:
     print('Avalição do produto...')
 
 elif opcao__definida == 9:
-    print('Pronto! Conheca nos mais na nossa comunidade...')
+    print('Pronto! Conheca-nos mais na nossa comunidade...')
 
 
 else:
-    print('Ops, houve algum erro, tente novamente!')
+   print('Não foi possível realizar o que deseja, tente novamente!')
+
+
