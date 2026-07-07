b7bdafbf108cc3d78cbd93a9c4c28c87bbaf3df4
andrade011 <santsandrade0@gmail.com>
2026-07-07 17:25:57 -0300

    downgrade

---
+++ pensamento_projeto_larissa__.py
@@ -0,0 +1,83 @@
+'''
+>>Projeto Açaiteria:
+'''
+
+
+
+# Ufa, quebrei a maldição
+# print('Olá, Mundo!')
+
+
+# Inicializando as variáveis para o produto 1 (vazio)
+p1_nome = 'Açaí refresco'
+p1_estoque = 100
+p1_preco = 15.00
+p1_validade = 25/10/2026
+p1_descrição = "acaí comum, delicie-se com o rerefresco do nosso açaí"
+
+# Iniciando as variáveis para  produto 2 (vazio)
+p2_nome = 'Açaí premium'
+p2_estoque = 70
+p2_preco = 20.00
+p2_validade = 25/11/2026
+p2_descrição = "acaí premium, perfeito para quem gosta do melhor" 
+
+#iniciando as variáveis para  produto 3 (vazio)
+p3_nome = 'Açaí experience' 
+p3_estoque = 50
+p3_preco =30.00
+p3_validade = 25/10/2026
+p3_descrição = "acaí experience, ideial para matar aquela vontade de algo especial" 
+
+# Isso é um comentário de linha única
+
+print('_'* 48 + '\n')
+print('Bem vindo ao sistema de vendas - Açaiteria!\n')
+print('1 - Cadastrar produto')
+print('2 - Listar produtos')
+print('3 - Realizar vendas')
+print('4 - Açaí')
+print('5 - Toppings')
+print('6 - Localização')
+print('7 - Contato')
+print('8 - Redes sociais')
+print('9-  Feedback')
+print('0 - Sair do sistema')
+print('\n--------------------------------------\n')
+
+opcao__definida = int(input('Digite a opcao deseja: '))
+
+if opcao__definida == 1:
+    print('Cadatrando produtos...')
+#faça a lógica para cadastrar o produto aqui,
+## e somente a inserção dos dados usando input e os tipos de dados.
+if p1_Açaí 
+elif opcao__definida == 2:
+    print('Listando produtos...')
+
+elif opcao__definida == 3:
+    print('Realizando Vendas...') 
+
+elif opcao__definida == 4:
+    print('Preparando Pedido...') 
+
+elif opcao__definida == 5:
+    print('Acrescentando os complementos...')
+
+elif opcao__definida == 6:
+    print('Localizando entrega...')
+
+elif opcao__definida == 7:
+    print('Entrando em contato...')
+        
+elif opcao__definida == 8:
+    print('Avalição do produto...')
+
+elif opcao__definida == 9:
+    print('Pronto! Conheca-nos mais na nossa comunidade...')
+
+
+else:
+   print('Não foi possível realizar o que deseja, tente novamente!')
+
+
